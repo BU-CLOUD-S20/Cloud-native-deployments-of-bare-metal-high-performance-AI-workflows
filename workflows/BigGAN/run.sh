4d85a95ad1d62d7e822e80ee81470507f27aed49
shawn3298317 <shawn3298317lin@gmail.com>
2020-02-12 12:30:19 -0500

    Able to build and start single-node, no-gpu training image

--- workflows/BigGAN/run.sh
+++ workflows/BigGAN/run.sh
@@ -2,25 +2,24 @@
 
 HOME=`pwd`
 WORK_DIR=${HOME}/work/
-DATA_ROOT="/data/"
+DATA_ROOT="/data/" # Need to mount data to this path when starting image
 
 # TODO: maybe move this to a configuration file?
 DATASET=ImageNet
 RESOLUTION=128
 DATASET_TYPE=ImageFolder
 TRAIN_FILE=${WORK_DIR}/trainfile_biggan_deep128_imagenet
-N=1
+N=1 # TODO: figure out what is the purpose of setting world size
 
 mkdir ${WORK_DIR} && cd ${WORK_DIR}
-# TODO: mount imagenet data to the image during runtime?
-# TODO: data_root should be configurable
-mkdir -p data && ln -s ${DATA_ROOT}/ImageNet data/ImageNet
-
-cd ${WORK_DIR}
 git clone https://github.com/shawn3298317/BigGAN-PyTorch.git
 cd ${WORK_DIR}/BigGAN-PyTorch/
 git checkout -b satori --track origin/satori
 
+# TODO: Separate the steps into multiple scripts and
+#       monitor the output of each intermediate step.
+
+cd ${WORK_DIR}
 echo "Making hdf5..."
 python3 ${WORK_DIR}/BigGAN-PyTorch/make_hdf5.py \
     --dataset ${DATASET} \
@@ -33,7 +32,10 @@ python3 ${WORK_DIR}/BigGAN-PyTorch/calculate_inception_moments.py \
     --dataset ${DATASET} \
     --resolution ${RESOLUTION} \
     --data_root ${DATA_ROOT} \
-    --dataset_type ${DATASET_TYPE}
+    --dataset_type ${DATASET_TYPE} \
+    --num_workers 1 \
+    --batch_size 4
+# TODO: set num_workers according to available cpu cores?
 echo "Inception moments done!"
 
 
@@ -42,7 +44,8 @@ echo "Start training bigGAN..."
 # mpirun -np ${N} \
 python3 ${WORK_DIR}/BigGAN-PyTorch/main.py \
     --model biggan_deep \
-    --dataset ${DATASET} --resolution ${RESOLUTION} --shuffle  --num_workers 8 --batch_size 2 \
+    --dataset ${DATASET} --resolution ${RESOLUTION} --shuffle  --num_workers 1 --batch_size 2 \
+    --data_root ${DATA_ROOT} \
     --dataset_type ${DATASET_TYPE} \
     --num_G_accumulations 1 --num_D_accumulations 1 \
     --num_D_steps 2 --G_lr 5e-5 --D_lr 2e-4 --D_B2 0.999 --G_B2 0.999 \
@@ -60,4 +63,4 @@ python3 ${WORK_DIR}/BigGAN-PyTorch/main.py \
     --world-size ${N}
     #--dist-url file://${TRAINFILE}
     #--multiprocessing-distributed \
-
+# TODO: Test distributed workflow

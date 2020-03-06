64913d05bab9bb9ea0edb6ca35b906c4d2bd6285
Jing Qin <qin_jin@live.com>
2020-03-06 04:54:07 -0500

    update: entrypoint.sh

--- workflows/BigGAN/gpu/entrypoint.sh
+++ workflows/BigGAN/gpu/entrypoint.sh
@@ -4,4 +4,7 @@ echo "import pty; pty.spawn('/bin/bash')" > /tmp/enter_into_bash.py
 python3 /tmp/enter_into_bash.py
 
 su pwrai
+
+echo 1 > bash accept-powerai-license.sh
+
 bash $HOME/workflows/BigGAN/gpu/run.sh

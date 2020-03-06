4fa4032ea07b0f0e5384d0b4c16eeb9f54195af9
Jing Qin <qin_jin@live.com>
2020-03-06 05:53:07 -0500

    update entrypoint.sh

--- workflows/BigGAN/gpu/entrypoint.sh
+++ workflows/BigGAN/gpu/entrypoint.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/usr/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 HOME=/home/pwrai
-
-echo "import pty; pty.spawn('/bin/bash')" > /tmp/enter_into_bash.py
-python3 /tmp/enter_into_bash.py
-
 su pwrai
 
 echo 1 > bash accept-powerai-license.sh

70111a5586dd66b2c3667d311e160dead88187ec
Jing Qin <qin_jin@live.com>
2020-03-06 04:49:36 -0500

    fix: openshift build failed

--- workflows/BigGAN/gpu/entrypoint.sh
+++ workflows/BigGAN/gpu/entrypoint.sh
@@ -1,4 +1,7 @@
 HOME=/home/pwrai
 
+echo "import pty; pty.spawn('/bin/bash')" > /tmp/enter_into_bash.py
+python3 /tmp/enter_into_bash.py
+
 su pwrai
-bash $HOME/workflows/BigGAN/run.sh
+bash $HOME/workflows/BigGAN/gpu/run.sh

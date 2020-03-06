5d0dae8e739b7cf3829c444383a18f95bd00105f
Jing Qin <qin_jin@live.com>
2020-03-06 15:24:03 -0500

    update: run.sh

--- workflows/BigGAN/gpu/run.sh
+++ workflows/BigGAN/gpu/run.sh
@@ -22,11 +22,18 @@ git clone https://github.com/alexandonian/BigGAN-PyTorch.git $WORK_DIR/$REPO
 cd $WORK_DIR/$REPO
 git checkout satori
 
-sh setup.sh
+# Start from directory of script
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+mkdir -p data/ImageNet && ln -s /data/ImageNet/ILSVRC2012/train data/ImageNet/train
+mkdir runs
+
+# Set up symlinks for the example notebooks
+bash notebooks/setup_notebooks.sh
 
 export PAMI_IBV_ADAPTER_AFFINITY=0
 export NCCL_SOCKET_IFNAME=ib
 
-sh ./make_hdf5.sh
-sh ./calc_inception_moments.sh
-sh ./run_biggan128_imagenet.sh
+bash $WORKDIR/atlas/workflows/BigGAN/gpu/make_hdf5.sh
+bash $WORKDIR/atlas/workflows/BigGAN/gpu/calc_inception_moments.sh
+bash $WORKDIR/atlas/workflows/BigGAN/gpu/run_biggan128_imagenet.sh
